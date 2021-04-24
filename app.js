89606f12a53f99e0bf75e38a01196afc36223ac4
Alex <adityalasika@gmail.com>
2021-04-24 14:52:17 +0700

    sasasa

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv/config')
 
-// Middleware mrxxxxxxxxx kondro
+// Middleware mrxxxxxxxxx kondro cvxbxcvxc
 app.use(bodyParser())
 app.use(cors())
 
