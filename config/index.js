f03ee456a8a40966f98836583174b090a7f8d854
Hrp <AmanHrp10>
2021-05-04 07:51:51 +0700

    pisah branch unupload file

--- config/index.js
+++ config/index.js
@@ -1,14 +1,1 @@
-const Cloud = require('@google-cloud/storage')
-const path = require('path')
-require('dotenv/config')
-const serviceKey = path.join(__dirname, '../key-new.json')
 
-const { Storage } = Cloud
-
-// Storage
-const storage = new Storage({
-  keyFilename: serviceKey,
-  projectId: "our-chess-310913",
-})
-
-module.exports = storage
